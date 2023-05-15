485706410b289b2e7f5f2ca49695f08da15e558d
risal098 <115255511+risal098@users.noreply.github.com>
2023-05-15 17:32:41 +0700

    Add files via upload

---
+++ static/cookie.js
@@ -0,0 +1,6 @@
+const username=localStorage.getItem("username");
+const password=localStorage.getItem("password");
+let cookies=parseInt(localStorage.getItem("cookies"));
+localStorage.removeItem("account");
+let cacheShowMyCookie=0;
+
